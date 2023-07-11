274eb9bcf60f59fd5ed96dca1ddab85a3e654910
Ernest Tan Jia Teng <57171675+SwordSlash39@users.noreply.github.com>
2023-07-11 19:54:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,6 @@ def train():
         reward_white, done_white = game.step(final_move)
         state_new = game.get_state()
 
-        # train short memory
-        agent.train_short_memory(state_old, final_move, reward_white, state_new, done_white)
-
         # remember
         agent.remember(state_old, final_move, reward_white, state_new, done_white)
 
@@ -41,29 +38,43 @@ def train():
         BLACK'S TURN
         ----------------------------------------------------------------
         """
-        if done_white:
+        if done_white:  # white wins or draw
             QLearn(game, agent)
             agent.saveModel('model.pth')
+            
+            if reward_white >= game.CHECKMATE:  # more means win (win reward always more than 200 else i stoopid)
+                agent.memory[-2][2] -= game.CHECKMATE
             continue
+        else:
+            # train short memory only IF it isnt checkmate
+            try:
+                agent.train_short_memory(state_old_black, final_move_black, reward_black, state_new_black, done_black)
+            except UnboundLocalError:
+                pass
 
-        state_old = state_new
+        state_old_black = state_new
 
         # get move
-        final_move = agent.get_action(game.board, "black")
+        final_move_black = agent.get_action(game.board, "black")
 
         # perform move and get new state
-        reward_black, done_black = game.step(final_move)
-        state_new = game.get_state()
+        reward_black, done_black = game.step(final_move_black)
+        state_new_black = game.get_state()
 
-        # train short memory
-        agent.train_short_memory(state_old, final_move, reward_black, state_new, done_black)
 
         # remember
-        agent.remember(state_old, final_move, reward_black, state_new, done_black)
+        agent.remember(state_old_black, final_move_black, reward_black, state_new_black, done_black)
 
         if done_black:
             QLearn(game, agent)
             agent.saveModel('model.pth')
+            
+            if reward_black >= game.CHECKMATE:  # more means win (win reward always more than 200 else i stoopid)
+                agent.memory[-2][2] -= game.CHECKMATE
+        else:
+            # train short memory
+            # UnboundedLocalError wont happen because already assigned earlier
+            agent.train_short_memory(state_old, final_move, reward_white, state_new, done_white)
 
 
 if __name__ == '__main__':
